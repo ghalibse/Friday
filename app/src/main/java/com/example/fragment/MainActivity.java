7918decca09957420cbcb2f9e29a14874b1beee3
Evin1- <edwin.hdz.12.0@gmail.com>
2016-07-28 14:42:55 -0400

    Add LoginFragment

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.a_main_frame, new LoginFragment())
+                .commit();
     }
 }
